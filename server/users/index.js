f17c6709ebeb6a07fad2b30936a7a1f887f6def7
rzambranoi <rzambranoi@edu.tecnocampus.cat>
2020-07-02 19:10:22 +0200

    login

--- server/users/index.js
+++ server/users/index.js
@@ -3,6 +3,9 @@ const router = Router();
 const db = require('../db/connection')
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
 
 
 const users = db.get('users');
@@ -39,7 +42,8 @@ router.post('/signup', [
                 const newUser = {
                     username: req.body.username,
                     password: hashPassword,
-                    email: req.body.email
+                    email: req.body.email,
+                    type: "basic"
                 }
                 users.insert(newUser).then(() => {
                     res.status(200).json({
@@ -50,6 +54,62 @@ router.post('/signup', [
             );
         }
     });
+});
+
+function error_login(res) {
+    return res.status(401).json({ error: "Username or password is not valid" });
+}
+
+
+router.post('/login', [
+
+    check("username").trim().isLength({ min: 5, max: 20 }).withMessage("must be at between 5 and 20 chars long"),
+    check("password")
+        .trim()
+        .isLength({ min: 8 }).withMessage("Must be at least 8 chars long")
+        .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])/, "i")
+        .withMessage("It must contain at least a lowercase, uppercase, a number and a special character ")
+        .notEmpty().withMessage("Password can not be empty")
+
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return error_login(res);
+    }
+
+    users.findOne({
+        username: req.body.username,
+    }).then(user => {
+        if (user) {
+            bcrypt.compare(req.body.password, user.password).then((result) => {
+
+                if (result) {
+                    const payload = {
+                        _id: user._id,
+                        username: user.username,
+                        email: user.email
+                    };
+
+                    jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '1d' }, (err, token) => {
+                        if (err) {
+                            return error_login(res);
+                        } else {
+                            return res.status(200).json({ token });
+                        }
+                    });
+
+                } else {
+                    return error_login(res);
+                }
+
+            });
+
+        } else {
+            return error_login(res);
+        }
+    });
+
+
 });
 
 module.exports = router;
